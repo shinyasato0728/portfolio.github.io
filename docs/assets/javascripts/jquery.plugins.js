fda2751a957213df08799723568aca70f72db137
cookboys <designsato0728@gmail.com>
2018-07-06 01:32:51 +0900

    menu-modify

--- docs/assets/javascripts/jquery.plugins.js
+++ docs/assets/javascripts/jquery.plugins.js
@@ -1,3 +1,17 @@
 $(function () {
 objectFitImages( 'img.ofi' );
 });
+
+var menuHeight = $("#langulages__wrapper").height();
+var startPos = 0;
+$(window).scroll(function(){
+  var currentPos = $(this).scrollTop();
+  if (currentPos > startPos) {
+    if($(window).scrollTop() >= 800) {
+      $("#langulages__wrapper").css("bottom", "-" + "600px");
+    }
+  } else {
+    $("#langulages__wrapper").css("bottom", 40 + "px");
+  }
+  startPos = currentPos;
+});
