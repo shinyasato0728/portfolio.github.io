be98251e026c21a84f16b88eae715b452e43ae6a
cookboys <designsato0728@gmail.com>
2018-07-07 07:30:31 +0900

    bem-linter-add

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   extends: "stylelint-config-primer",
+  "plugins": [
+    "stylelint-selector-bem-pattern"
+  ],
   rules: {
+    "plugin/selector-bem-pattern": {
+      "componentName": "[A-Z]+",
+      "componentSelectors": {
+        "initial": "^\\.{componentName}(?:-[a-z]+)?$",
+        "combined": "^\\.combined-{componentName}-[a-z]+$"
+      },
+      "utilitySelectors": "^\\.util-[a-z]+$"
+    },
     "indentation": 2,
     "declaration-block-semicolon-newline-after": "always-multi-line",
     "block-opening-brace-newline-after": "never-multi-line",
