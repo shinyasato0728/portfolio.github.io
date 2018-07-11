522d581076e67932ab25b0377bb37024bdf5406a
cookboys <designsato0728@gmail.com>
2018-07-11 23:46:48 +0900

    click__sound

--- docs/assets/javascripts/portfolio.js
+++ docs/assets/javascripts/portfolio.js
@@ -5,19 +5,18 @@ $(function () {
   objectFitImages( 'img.ofi' );
 });
 
-function sound() {
+var userAgent = window.navigator.userAgent.toLowerCase();
+var ie = document.getElementById("ie__message");
+ie.style.display ="none";
+if(userAgent.indexOf('msie') > -1 || userAgent.indexOf('trident') > -1) {
+  ie.classList.add('ie__alert');
+  ie.style.display ="block";
+};
+
+$(".click__sound").on("click", function() {
   var id = 'sound';
   if( typeof( document.getElementById( id ).currentTime ) != 'undefined' ) {
     document.getElementById( id ).currentTime = 0;
   }
   document.getElementById( id ).play();
-}
-
-var userAgent = window.navigator.userAgent.toLowerCase();
-var ie = document.getElementById("ie__message");
-ie.style.display ="none";
-
-if(userAgent.indexOf('msie') > -1 || userAgent.indexOf('trident') > -1) {
-  ie.classList.add('ie__alert');
-  ie.style.display ="block";
-}
+});
